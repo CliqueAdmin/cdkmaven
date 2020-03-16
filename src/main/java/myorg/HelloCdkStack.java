fee14efa4190bcc138032b40d14ef5d728cee2c7
Clique Admin <Cliquedmn@gmail.com>
2020-03-15 17:06:46 -0700

    Add api gateway to the lambda custom pojo

--- src/main/java/myorg/HelloCdkStack.java
+++ src/main/java/myorg/HelloCdkStack.java
@@ -4,13 +4,13 @@ import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.apigateway.*;
 import software.amazon.awscdk.services.dynamodb.*;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.IEventSource;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.lambda.eventsources.SqsEventSource;
-import software.amazon.awscdk.services.sqs.DeadLetterQueue;
 import software.amazon.awscdk.services.sqs.Queue;
 
 import java.util.ArrayList;
@@ -60,6 +60,16 @@ public class HelloCdkStack extends Stack {
         SqsEventSource sqsEventSource = SqsEventSource.Builder.create(queue).batchSize(1).build();
         List<IEventSource> eventSourceList = new ArrayList<>();
         eventSourceList.add(sqsEventSource);
+//
+//        Map<String, String> lambdaIntegrationMap = new HashMap<String, String>();
+//        lambdaIntegrationMap.put("application/json", "{ \"statusCode\": \"200\" }");
+
+        List<EndpointType> endpointTypeList = new ArrayList<>();
+        endpointTypeList.add(EndpointType.REGIONAL);
+        RestApi restApi = RestApi.Builder.create(this, "hello-rest-api").
+                endpointTypes(endpointTypeList).restApiName("hello-rest-api").deploy(true)
+                .build();
+
 
         final Function itemsreplenishment = Function.Builder.create(this, "ItemsReplenishmentController")
                 .runtime(Runtime.JAVA_8)    // execution environment
@@ -71,12 +81,24 @@ public class HelloCdkStack extends Stack {
                 .deadLetterQueue(itemDlq)
                 .build();
 
+
         final Function itemsreplenishmentApi = Function.Builder.create(this, "ItemsReplenishmentApi")
                 .runtime(Runtime.JAVA_8)    // execution environment
                 .code(Code.fromAsset("cdk.out/itemsreplenishment-1.0.zip"))  // code loaded from the "lambda" directory
                 .handler("itemsreplenishment.ItemsReplenishmentController")        // file is "hello", function is "handler"
                 .environment(environmentVariables)
                 .build();
+
+        List<MethodResponse> methodResponses = new ArrayList<>();
+        methodResponses.add(MethodResponse.builder().statusCode("200").build());
+
+        IntegrationResponse integrationResponse = IntegrationResponse.builder().statusCode("200").build();
+        List<IntegrationResponse> responses = new ArrayList<>();
+        responses.add(integrationResponse);
+        restApi.getRoot().addMethod("POST", LambdaIntegration.Builder.
+                create(itemsreplenishmentApi).proxy(false).
+                integrationResponses(responses).build(), MethodOptions.builder().methodResponses(methodResponses).build());
+        ;
 
 
     }
