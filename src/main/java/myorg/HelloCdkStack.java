69c2d0b47f73e5048906199f8ce4c36f5c41b975
Clique Admin <Cliquedmn@gmail.com>
2020-03-18 00:17:44 -0700

    Add sqs->lambda->step->sqs->lambda fun proof of concept

--- src/main/java/myorg/HelloCdkStack.java
+++ src/main/java/myorg/HelloCdkStack.java
@@ -1,17 +1,20 @@
 package myorg;
 
-import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.RemovalPolicy;
-import software.amazon.awscdk.core.Stack;
-import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.core.*;
 import software.amazon.awscdk.services.apigateway.*;
 import software.amazon.awscdk.services.dynamodb.*;
+import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.IEventSource;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.lambda.eventsources.SqsEventSource;
 import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.stepfunctions.Chain;
+import software.amazon.awscdk.services.stepfunctions.StateMachine;
+import software.amazon.awscdk.services.stepfunctions.Task;
+import software.amazon.awscdk.services.stepfunctions.TaskInput;
+import software.amazon.awscdk.services.stepfunctions.tasks.SendToQueue;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -50,19 +53,16 @@ public class HelloCdkStack extends Stack {
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
         Table dynamodbTable = new Table(this, "IGHItems", tableProps);
-        Map<String, String> environmentVariables = new HashMap<String, String>();
-        environmentVariables.put("BUCKET", "bucket.getBucketName()");
 
 
-        Queue queue = Queue.Builder.create(this, "itemqueue").queueName("item-replenish-queue").build();
+        Queue queue = Queue.Builder.create(this, "itemqueue").queueName("item-replenish-queue").
+                visibilityTimeout(Duration.minutes(10))
+                .build();
         Queue itemDlq = Queue.Builder.create(this, "itemdlqueue").queueName("item-replenish-dlq").build();
 
         SqsEventSource sqsEventSource = SqsEventSource.Builder.create(queue).batchSize(1).build();
         List<IEventSource> eventSourceList = new ArrayList<>();
         eventSourceList.add(sqsEventSource);
-//
-//        Map<String, String> lambdaIntegrationMap = new HashMap<String, String>();
-//        lambdaIntegrationMap.put("application/json", "{ \"statusCode\": \"200\" }");
 
         List<EndpointType> endpointTypeList = new ArrayList<>();
         endpointTypeList.add(EndpointType.REGIONAL);
@@ -71,22 +71,10 @@ public class HelloCdkStack extends Stack {
                 .build();
 
 
-        final Function itemsreplenishment = Function.Builder.create(this, "ItemsReplenishmentController")
-                .runtime(Runtime.JAVA_8)    // execution environment
-                .code(Code.fromAsset("cdk.out/itemsreplenishment-1.0.zip"))  // code loaded from the "lambda" directory
-                .handler("itemsreplenishment.events.ItemsReplenishmentSqsHandler")        // file is "hello", function is "handler"
-                .environment(environmentVariables)
-                .events(eventSourceList)
-                .deadLetterQueueEnabled(true)
-                .deadLetterQueue(itemDlq)
-                .build();
-
-
         final Function itemsreplenishmentApi = Function.Builder.create(this, "ItemsReplenishmentApi")
                 .runtime(Runtime.JAVA_8)    // execution environment
                 .code(Code.fromAsset("cdk.out/itemsreplenishment-1.0.zip"))  // code loaded from the "lambda" directory
                 .handler("itemsreplenishment.ItemsReplenishmentController")        // file is "hello", function is "handler"
-                .environment(environmentVariables)
                 .build();
 
         List<MethodResponse> methodResponses = new ArrayList<>();
@@ -98,8 +86,43 @@ public class HelloCdkStack extends Stack {
         restApi.getRoot().addMethod("POST", LambdaIntegration.Builder.
                 create(itemsreplenishmentApi).proxy(false).
                 integrationResponses(responses).build(), MethodOptions.builder().methodResponses(methodResponses).build());
-        ;
+        TaskInput taskInput = TaskInput.fromDataAt("$.hello");
 
+        Task submitJob =
+                Task.Builder.create(this, "Submit Job")
+                        .task(SendToQueue.Builder.create(queue).messageBody(taskInput).build())
+//                        .inputPath("$.hello")
+//                        .resultPath("$.status")
+                        .build();
+        Chain chain =
+                Chain.start(submitJob);
+        StateMachine stateMachine = StateMachine.Builder.create(this, "StateMachine")
+                .definition(chain)
+                .stateMachineName("Hello-State")
+                .timeout(Duration.seconds(30))
+                .build();
 
+        Map<String, String> environmentVariables = new HashMap<String, String>();
+        environmentVariables.put("STATE_MACHINE_ARN", stateMachine.getStateMachineArn());
+
+        List<String> resources = new ArrayList<>();
+        resources.add("*");
+        List<String> actions = new ArrayList<>();
+        actions.add("states:StartExecution");
+
+        List<PolicyStatement> policyStatements = new ArrayList<>();
+        policyStatements.add(PolicyStatement.Builder.create().resources(resources).actions(actions).build());
+        final Function itemsreplenishment = Function.Builder.create(this, "ItemsReplenishmentController")
+                .runtime(Runtime.JAVA_8)    // execution environment
+                .code(Code.fromAsset("cdk.out/itemsreplenishment-1.0.zip"))  // code loaded from the "lambda" directory
+                .handler("itemsreplenishment.events.ItemsReplenishmentSqsHandler")        // file is "hello", function is "handler"
+                .environment(environmentVariables)
+                .initialPolicy(policyStatements)
+                .events(eventSourceList)
+                .memorySize(256)
+                .timeout(Duration.minutes(10))
+                .deadLetterQueueEnabled(true)
+                .deadLetterQueue(itemDlq)
+                .build();
     }
 }
